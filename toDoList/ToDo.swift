77f4b778fdb933f073503f0dfca072082c0c7d49
Isabella <isabella.presser@gmail.com>
2020-07-14 11:11:46 -0400

    add button

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Isabella Presser on 7/14/20.
+//  Copyright © 2020 Isabella Presser. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
